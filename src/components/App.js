c086ba1bc6d92dc533eaca18b458f2f361877c53
iamchacha021 <iamchacha021@gmail.com>
2023-01-18 12:23:16 +0300

    1. Display product based on categor in shopping list 2. Add light mode and dark mode in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,20 @@ import itemData from "../data/items";
 import { useState } from "react";
 
 function App() {
+const [isLight, setIsLight] = useState(true)
+const handleLightMode=(ev)=>{
+  setIsLight(isLight=> !isLight)
+}
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = true ? "App light" : "App dark"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleLightMode}>{isLight? "Dark Mode":"Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [category, setCategory] = useState("All")
+  const handleOnChange=(ev)=>{
+    setCategory(ev.target.value)
+  }
+  const displayedItems = items.filter((item)=>{
+    if(category === "All"){
+      return(category)
+    }else{
+      return (item.category=== category)
+    }
+    
+  })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleOnChange} value={category } name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayedItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
